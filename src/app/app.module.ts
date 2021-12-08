0cd76ffda32d928caaae0cc7ba800ef354795a09
AmberKors <aa.kors@belastingdienst.nl>
2021-12-08 12:32:51 +0100

    Added components, services and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UserRegistrationComponent} from './user-registration/user-registration.component';
+import {ProductRegistrationComponent} from './product-registration/product-registration.component';
+import {ProductListComponent} from './product-list/product-list.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {ShoppingCartComponent} from './shopping-cart/shopping-cart.component';
+import {HomePage} from "./pages/home.page";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import {routes} from "./app.routes";
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePage,
+    LoginComponent,
+    UserRegistrationComponent,
+    ProductRegistrationComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ShoppingCartComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import {Route} from '@angular/router';
+import {HomePage} from './pages/home.page';
+
+import {UserRegistrationComponent} from "./user-registration/user-registration.component";
+import {LoginComponent} from "./login/login.component";
+import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
+
+export const routes: Route[] = [
+  {path: '', component: HomePage},
+  {path: 'home', component: HomePage},
+  {path: 'login', component: LoginComponent},
+  {path: 'user-registration', component: UserRegistrationComponent},
+  {path: 'shopping-cart', component: ShoppingCartComponent}
+];
