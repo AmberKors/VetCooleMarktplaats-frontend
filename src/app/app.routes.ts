47fed8f17ddb7435d0a6f76b18226a0c6bea04c1
AmberKors <aa.kors@belastingdienst.nl>
2021-12-08 16:05:00 +0100

    [STRY-0046] Added routing for product detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import {HomePage} from './pages/home.page';
 import {UserRegistrationComponent} from "./user-registration/user-registration.component";
 import {LoginComponent} from "./login/login.component";
 import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
+import {ProductDetailComponent} from "./product-detail/product-detail.component";
 
 export const routes: Route[] = [
   {path: '', component: HomePage},
   {path: 'login', component: LoginComponent},
   {path: 'user-registration', component: UserRegistrationComponent},
-  {path: 'shopping-cart', component: ShoppingCartComponent}
+  {path: 'shopping-cart', component: ShoppingCartComponent},
+  {path: 'products/:id', component: ProductDetailComponent}
 ];
