b3fa56153f34f532ce1441106c31ed145ca85ffa
Mschuppers <m.schuppers@belastingdienst.nl>
2021-12-10 09:43:22 +0100

    Customer registration is remembered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {LoginService} from "./services/login.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'Vet Coole Marktplaats';
+
+  loggedIn$ = this.service.loggedIn$;
+  loggedOut$ = this.service.loggedOut$;
+
+  loggedIn = false;
+  logLabel = 'Login';
+  logLink = 'login';
+  loggedInMessage = 'Not logged in.';
+
+
+  constructor(private router: Router,
+              private service: LoginService) {
+    this.loggedIn$.subscribe((u) => {
+      this.loggedIn = true;
+      this.logLabel = 'Logout';
+      this.logLink = 'logout';
+      this.loggedInMessage = `Logged in as ${u}.`;
+    });
+
+    this.loggedOut$.subscribe((u) => {
+      this.loggedIn = false;
+      this.logLabel = 'Login';
+      this.logLink = 'login';
+      this.loggedInMessage = 'Not logged in.';
+    });
+  }
+
+  isLoggedIn(): boolean {
+    return this.loggedIn;
+  }
+
+  logout(): void {
+    this.service.logout();
+    this.router.navigate(['/login']);
+  }
 }
