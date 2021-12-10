e140993efc32719d149e8aa5830ecf8d173e3751
Mschuppers <m.schuppers@belastingdienst.nl>
2021-12-10 15:52:45 +0100

    Fixed minor routing bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import {Route} from '@angular/router';
 import {HomePage} from './pages/home.page';
 import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
 import {ProductDetailComponent} from "./product-detail/product-detail.component";
+import {ProductRegistrationComponent} from "./product-registration/product-registration.component";
 
 export const routes: Route[] = [
   {path: '', component: HomePage},
   {path: 'shopping-cart', component: ShoppingCartComponent},
   {path: 'products/:id', component: ProductDetailComponent},
+  {path: 'nieuwproduct', component: ProductRegistrationComponent},
 ];
