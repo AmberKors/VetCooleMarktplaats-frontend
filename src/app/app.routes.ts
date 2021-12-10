93994144d4d6b641cf65fad76c3118cfd1130c52
AmberKors <aa.kors@belastingdienst.nl>
2021-12-10 14:12:32 +0100

    push login route issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,12 @@
 import {Route} from '@angular/router';
 import {HomePage} from './pages/home.page';
-
-import {UserRegistrationComponent} from "./user-registration/user-registration.component";
-import {LoginComponent} from "./login/login.component";
 import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
 import {ProductDetailComponent} from "./product-detail/product-detail.component";
+import {LogoutComponent} from "./login/logout.component";
 
 export const routes: Route[] = [
   {path: '', component: HomePage},
-  {path: 'login', component: LoginComponent},
-  {path: 'user-registration', component: UserRegistrationComponent},
+  {path: 'logout', component: LogoutComponent},
   {path: 'shopping-cart/:id', component: ShoppingCartComponent},
   {path: 'products/:id', component: ProductDetailComponent}
 ];
